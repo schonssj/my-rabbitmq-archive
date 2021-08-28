775e6640c73e0d2a7f9fe26b0a782d7152844605
schonssj <jvschons@gmail.com>
2021-08-28 14:32:11 -0300

    Add project files.

---
+++ MyRabbitMQArchive.HelloWorld/MyRabbitMQArchive.HelloWorld.Receiver/MessageReceiver.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace MyRabbitMQArchive.HelloWorld
+{
+    public class MessageReceiver
+    {
+        public static void Main()
+        {
+            var connectionFactory = new ConnectionFactory() { HostName = "localhost" };
+
+            using var connection = connectionFactory.CreateConnection();
+            using var channel = connection.CreateModel();
+            {
+                channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                var eventingBasicConsumer = new EventingBasicConsumer(channel);
+
+                eventingBasicConsumer.Received += (model, ea) =>
+                {
+                    var parsedBody = ea.Body.ToArray();
+                    var receivedMessage = Encoding.UTF8.GetString(parsedBody);
+                    Console.WriteLine($"Received message: {receivedMessage}");
+                };
+
+                channel.BasicConsume(queue: "hello", autoAck: true, consumer: eventingBasicConsumer);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ MyRabbitMQArchive.HelloWorld/MyRabbitMQArchive.HelloWorld.Sender/MessageSender.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace MyRabbitMQArchive.HelloWorld
+{
+    public class MessageSender
+    {
+        public static void Main()
+        {
+            var message = "Hello World!";
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            var parsedBody = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: string.Empty, routingKey: "hello", basicProperties: null, body: parsedBody);
+
+            Console.WriteLine($"Sent body: {message}");
+            Console.ReadLine();
+        }
+    }
+}
