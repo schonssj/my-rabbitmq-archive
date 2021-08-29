4dd1af632dba8e29d499aa7a92201ace77deadbb
schonssj <jvschons@gmail.com>
2021-08-28 21:30:02 -0300

    created a fake agent to simulate a real-world task

---
+++ MyRabbitMQArchive.WorkQueues/MyRabbitMQArchive.WorkQueues.NewTask/NewTask.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace MyRabbitMQArchive.WorkQueues.NewTask
+{
+    public class NewTask
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            {
+                channel.QueueDeclare(queue: "task_queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                var message = GetMessage(args);
+                var parsedBody = Encoding.UTF8.GetBytes(message);
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+
+                channel.BasicPublish(exchange: string.Empty, routingKey: "task_queue", basicProperties: properties, body: parsedBody);
+
+                Console.WriteLine($"Sent body: {message}");
+            }
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? String.Join(" ", args) : "Hello World!");
+        }
+    }
+}
