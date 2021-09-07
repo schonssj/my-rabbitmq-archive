8958c912f904ad8023971d5c390ab3d31e750cdd
schonssj <jvschons@gmail.com>
2021-09-06 22:58:45 -0300

    added an argument handler; changed the exchange's type and name; configured the routing key to dinamycally choose a binding based on the input

---
+++ MyRabbitMQArchive.Routing/MyRabbitMQArchive.Routing.EmitLogDirect/EmitLogDirect.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace MyRabbitMQArchive.Routing.EmitLogDirect
+{
+    public class EmitLogDirect
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            {
+                channel.ExchangeDeclare(exchange: "direct_logs", type: "direct");
+
+                var severity = args.Length > 0 ? args[0] : "info";
+                var message = args.Length > 1 ? string.Join(" ", args.Skip(1).ToArray()) : "Hello World!";
+                var parsedBody = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "direct_logs", routingKey: severity, basicProperties: null, body: parsedBody);
+
+                Console.WriteLine("Sent '{0}':'{1}'", severity, message);
+            }
+        }
+    }
+}
